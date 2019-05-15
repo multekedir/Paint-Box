10134677d46c52cbe58e5b58fcae61a4d6d67bcc
Multezem Kedir <kedirmultezem@gmail.com>
2019-05-15 11:39:31 -0700

    make it better

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config(object):
     DEVELOPMENT = True
     SECRET_KEY = 'why-wh4#*&kfkajdhfkywhyw2hywhy'
     FLASK_SECRET = SECRET_KEY
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://ugcydynpbjresa:6f6cfebc2b4b5ec69b70263437d8c144b6ee2a395450b094a3ea6389ce4fd0a0@ec2-23-21-106-241.compute-1.amazonaws.com:5432/d6l77h6pc3kd7d'
     SQLALCHEMY_ECHO = True
